005d15024f3ec3d349b9ddb1ff4eeebb8feb8630
devsecopstrainer <devsecopstrainer@gmail.com>
2026-02-15 10:20:02 +0530

    first commit

---
+++ src/main/java/com/idream/SecondController.java
@@ -0,0 +1,22 @@
+package com.idream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second")
+public class SecondController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SecondController.class);
+
+	@PostMapping("/get-data")
+	public String getData() {
+		LOGGER.info("*----------- Second micro service calld -----------*");
+		String resp = "Welcome from Second Micro Service";
+		LOGGER.info("Response sent from micro service B -> " + resp);
+		return resp;
+	}
+}
